c72e823b7cf6d7bd5f2bceaabd027925e50e370f
jpedrobacker <jpedrobacker@outlook.com>
2025-03-31 15:48:33 -0300

    q1 sql done

--- consulta_dados_questao1.sql
+++ consulta_dados_questao1.sql
@@ -1,4 +1,4 @@
--- Questao 1 a)
+-- Q1 a)
 SELECT  sc.name AS school_name, st.enrolled_at, COUNT(st.student_id) AS total_students, SUM(c.price) AS total_price
 FROM students st
 JOIN courses c ON st.course_id = c.course_id
@@ -7,4 +7,17 @@ WHERE c.name ILIKE 'DATA%'
 GROUP BY sc.name, st.enrolled_at
 ORDER BY st.enrolled_at DESC;
 
--- Questao 1 b)
+-- Q1 b)
+SELECT  sc.name AS school_name, 
+    st.enrolled_at, 
+    COUNT(st.student_id) AS total_students, 
+    SUM(c.price) AS total_price,
+    SUM(COUNT(st.student_id)) OVER (PARTITION BY sc.name ORDER BY st.enrolled_at) AS acumulative_sum,
+    AVG(COUNT(st.student_id)) OVER (PARTITION BY sc.name ORDER BY st.enrolled_at ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS moving_avg_7_days,
+    AVG(COUNT(st.student_id)) OVER (PARTITION BY sc.name ORDER BY st.enrolled_at ROWS BETWEEN 29 PRECEDING AND CURRENT ROW) AS moving_avg_30_days
+FROM students st
+JOIN courses c ON st.course_id = c.course_id
+JOIN schools sc ON c.school_id = sc.school_id
+WHERE c.name ILIKE 'DATA%'
+GROUP BY sc.name, st.enrolled_at, st.student_id
+ORDER BY st.enrolled_at DESC;
